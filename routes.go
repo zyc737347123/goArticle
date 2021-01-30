d56ca551a4fe75244ae4fa5352fcc24e75bdbad9
zyc737347123 <zyc737347123@163.com>
2021-01-30 13:48:17 +0800

    feat: add index page handle

--- routes.go
+++ routes.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Handle the index route
 	router.GET("/", showIndexPage)
-}
-
-func showIndexPage(c *gin.Context) {
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HTTP status to 200 (OK)
-		http.StatusOK,
-		// Use the index.html template
-		"index.html",
-		// Pass the data that the page uses (in this case, 'title')
-		gin.H{
-			"title": "Home Page",
-			"Body": "fuck the world",
-		},
-	)
 }
