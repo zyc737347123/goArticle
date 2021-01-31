66f2698ed8fbb74241439b88eecb63c9a7f62976
zyc737347123 <zyc737347123@163.com>
2021-01-31 20:32:07 +0800

    feat: add article page unit test

--- handlers.article.go
+++ handlers.article.go
@@ -9,35 +9,35 @@ import (
 func showIndexPage(c *gin.Context) {
 	articles := getAllArticles()
 
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HTTP status to 200 (OK)
-		http.StatusOK,
-		// Use the index.html template
-		"index.html",
-		// Pass the data that the page uses (in this case, 'title')
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	render(c, gin.H{
+		"title": "Home Page",
+		"payload": articles,
+	}, "index.html")
 }
 
 func getArticle(c *gin.Context) {
 	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
 		if article, err := getArticleByID(articleID); err == nil {
-			c.HTML(
-				http.StatusOK,
-				"article.html",
-				gin.H{
-					"title":   article.Title,
-					"payload": article,
-				},
-			)
+			render(c, gin.H{
+				"title": article.Title,
+				"payload": article,
+			}, "article.html")
 		} else {
 			c.AbortWithError(http.StatusNotFound, err)
 		}
 	} else {
 		c.AbortWithError(http.StatusNotFound, err)
+	}
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
 	}
 }
